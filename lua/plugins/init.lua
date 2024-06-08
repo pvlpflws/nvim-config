55b0a1316740cffa9613b59994cc81e6581958e1
Pavel Kurach <pavel.kurach1@gmail.com>
2024-06-09 00:07:19 +0200

    wip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -101,16 +101,10 @@ return {
   },
 
   {
-    "m4xshen/hardtime.nvim",
-    lazy = false,
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {},
-  },
-
-  {
     "tris203/precognition.nvim",
     lazy = false,
   },
+
   {
     "numToStr/Comment.nvim",
     lazy = false,
